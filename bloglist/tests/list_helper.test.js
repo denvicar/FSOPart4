d170e5fd18f61df2d4f88ff5d98ffcc9185c6537
Ciro Faenza <rkumura3@gmail.com>
2021-10-31 12:13:36 +0100

    exercise4.4

--- bloglist/tests/list_helper.test.js
+++ bloglist/tests/list_helper.test.js
@@ -3,3 +3,46 @@ const listHelper = require('../utils/list_helper')
 test('dummy returns 1', () => {
     expect(listHelper.dummy([])).toBe(1)
 })
+
+describe('total likes', () => {
+    test('of empty list is zero', () => {
+        expect(listHelper.totalLikes([])).toBe(0)
+    })
+
+    test('when a list has one blog returns the likes of that', () => {
+        const blog = [
+            {
+                title: 'mock title',
+                url: 'mockurl',
+                author: 'mockauthor',
+                likes: 500
+            },]
+
+        expect(listHelper.totalLikes(blog)).toBe(500)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const blogList = [
+            {
+                title: 'title1',
+                author: 'author1',
+                url: 'url1',
+                likes: 350
+            },{
+                title: 'title2',
+                author: 'author2',
+                url: 'url2',
+                likes: 800
+            },{
+                title: 'title3',
+                author: 'author3',
+                url: 'url3',
+                likes: 100
+            }
+        ]
+
+        expect(listHelper.totalLikes(blogList)).toBe(1250)
+    })
+
+
+})
