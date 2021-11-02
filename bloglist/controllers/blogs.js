677a3f5c2b4d4d511962c15a3f77098e95e13b51
Ciro Faenza <rkumura3@gmail.com>
2021-11-02 10:48:14 +0100

    exercise4.20

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -4,28 +4,20 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const config = require('../utils/config')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 appRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
     response.json(blogs)
   })
   
 appRouter.post('/', async (request, response) => {
-    const token = getTokenFrom(request)
+    const token = request.token
     const decodedToken = jwt.verify(token, config.SECRET)
 
     if(!decodedToken || !decodedToken.id) {
         return response.status(401).json({error: 'invalid or missing token'})
     }
     const user = await User.findById(decodedToken.id)
-    
+
     const post = request.body
     const blog = new Blog({
         author: post.author,
